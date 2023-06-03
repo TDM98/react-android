9567f25cd005bbb6cc0fbe5fe3d4b3d3d07ff7a2
MEDICBACLIEU\it04 <tdminhct@gmail.com>
2023-06-03 17:03:56 +0700

    5pm 3/6

--- src/screens/UserInfo.js
+++ src/screens/UserInfo.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, {useContext, useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
 import {
   Button,
   FlatList,
@@ -8,32 +8,34 @@ import {
   TouchableOpacity,
   View,
   TextInput,
-  Image
+  Image,
+  ScrollView
 } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
-import {useState} from 'react/cjs/react.development';
-import {BASE_URL} from '../config';
-import {primary, borderColor} from '../screens/color';
-import {AuthContext} from '../context/AuthContext'
+import { useState } from 'react/cjs/react.development';
+import { BASE_URL } from '../config';
+import { primary, borderColor } from '../screens/color';
+import { AuthContext } from '../context/AuthContext'
 
-const UserInfo = ({navigation, route}) => {
+const UserInfo = ({ navigation, route }) => {
   const [users, setUsers] = useState({});
-  const {user, logout, loading} = useContext(AuthContext);
-const [email,setEmail] = useState({});
-const [firstName,setFirstname]=useState({});
-const [lastName,setLastname]=useState({});
-const [fullName,setFullname]=useState({});
-const [login,setLogin]=useState({});
-const[password,setPassword]=useState({});
-const handleSubmit = () => {
+  const { user, logout, loading } = useContext(AuthContext);
+  const [email, setEmail] = useState({});
+  const [firstName, setFirstname] = useState({});
+  const [lastName, setLastname] = useState({});
+  const [fullName, setFullname] = useState({});
+  const [login, setLogin] = useState({});
+  const [password, setPassword] = useState({});
+  const handleSubmit = () => {
 
-}
+  }
   const getUsers = () => {
     axios
       .get(`${BASE_URL}/users/get-current-user-login`, {
         headers: {
-            
-            Authorization: `Bearer ${user.id_token}`},
+
+          Authorization: `Bearer ${user.id_token}`
+        },
       })
       .then(res => {
 
@@ -50,76 +52,83 @@ const handleSubmit = () => {
   }, [route.params?.userss]);
 
   return (
-      <View style={styles.container}>
-      <View style={styles.avatarContainer}>
-        <Image
-          style={styles.avatar}
-          source={{uri: 'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg'}}
-        />
-        <TouchableOpacity style={styles.changeAvatarButton} onPress={() => {/* open image picker */}}>
-          <Text style={styles.changeAvatarButtonText}>Change Avatar</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.form}>
-        <Text style ={styles.label} >First Name</Text>
-        <TextInput
-        style={styles.input}
-        placeholder='Enter First Name'
-        value={users.firstName}
-        onChangeText={setFirstname}
-        />
-        <Text style ={styles.label} >Last Name</Text>
-        <TextInput
-        style={styles.input}
-        placeholder='Enter Last Name'
-        value={users.lastName}
-        onChangeText={setLastname}
-        />
-        <Text style={styles.label}>Full Name</Text>
-        <TextInput
-        style={styles.input}
-        placeholder='Enter Full Name'
-        value={users.fullName}
-        onChangeText={setFullname}
-        />
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Email"
-          value={users.email}
-          onChangeText={setEmail}
-        />
-        <Text style={styles.label}>User name</Text>
-        <TextInput
-          editable={false}
-          style={styles.input}
-          value={users.login}
-          onChangeText={setEmail}
-        />
-         <Text style={styles.label}>Password</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Password"
-          value={users.password}
-          onChangeText={setPassword}
-        />
-        <TouchableOpacity style={styles.button} onPress={() => handleSubmit({firstName, lastName, fullName, email, password})}>
-          <Text style={styles.buttonText}>Submit</Text>
-        </TouchableOpacity>
-      </View>
-
+    <View style={styles.container}>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}>
+        <View style={styles.avatarContainer}>
+          <Image
+            style={styles.avatar}
+            source={{ uri: 'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg' }}
+          />
+          <TouchableOpacity style={styles.changeAvatarButton} onPress={() => {/* open image picker */ }}>
+            <Text style={styles.changeAvatarButtonText}>Change Avatar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.form}>
+          <Text style={styles.label} >First Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='Enter First Name'
+            value={users.firstName}
+            onChangeText={setFirstname}
+          />
+          <Text style={styles.label} >Last Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='Enter Last Name'
+            value={users.lastName}
+            onChangeText={setLastname}
+          />
+          <Text style={styles.label}>Full Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='Enter Full Name'
+            value={users.fullName}
+            onChangeText={setFullname}
+          />
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Email"
+            value={users.email}
+            onChangeText={setEmail}
+          />
+          <Text style={styles.label}>User name</Text>
+          <TextInput
+            editable={false}
+            style={styles.input}
+            value={users.login}
+            onChangeText={setEmail}
+          />
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Password"
+            value={users.password}
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity style={styles.button} onPress={() => handleSubmit({ firstName, lastName, fullName, email, password })}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
-    );
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignSelf: 'center',
     justifyContent: 'center',
+
   },
   form: {
-    width: '80%',
+    width: '90%',
+    alignSelf: 'center',
+    justifyContent: 'center',
   },
   label: {
     marginTop: 20,
@@ -137,6 +146,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     paddingVertical: 10,
     paddingHorizontal: 20,
+    marginBottom:50
   },
   buttonText: {
     color: '#fff',
